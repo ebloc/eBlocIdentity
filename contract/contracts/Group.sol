4100cbaf9fed2008ef16ea5890cadceec3e70ec1
Berk Erol <berk.erol@boun.edu.tr>
2019-03-10 19:56:39 +0300

    Update Root as proxy library
    Add DispatcherStorage & Dispatcher & RootInterface & RootImplementation contracts

--- contract/contracts/Group.sol
+++ contract/contracts/Group.sol
@@ -211,7 +211,50 @@ contract Group {
     }
 }
 
-contract Root {
+contract DispatcherStorage {
+
+    address public lib;
+    mapping(address => bool) private owners;
+
+    constructor(address[] memory _owners) public {
+        for (uint i = 0; i < _owners.length; i++) {
+            owners[_owners[i]] = true;
+        }
+    }
+
+    modifier onlyOwners() {
+        require(owners[msg.sender], "Caller is not owner.");
+        _;
+    }
+
+    function upgrade(address newLib) public onlyOwners {
+        lib = newLib;
+    }
+}
+
+contract Dispatcher {
+
+    function() external {
+        DispatcherStorage dispatcherStorage = DispatcherStorage(0x1111222233334444555566667777888899990000);
+        address target = dispatcherStorage.lib();
+
+        assembly {
+            calldatacopy(0x0, 0x0, calldatasize)
+            let success := delegatecall(sub(gas, 10000), target, 0x0, calldatasize, 0, 0)
+            let retSz := returndatasize
+            returndatacopy(0, 0, retSz)
+            switch success
+            case 0 {
+                revert(0, retSz)
+            }
+            default {
+                return(0, retSz)
+            }
+        }
+    }
+}
+
+library RootInterface {
 
     struct OwnerRemovalVoter {
         mapping(address => bool) voters;
@@ -222,146 +265,251 @@ contract Root {
         address addr;
     }
 
-    uint256 private ownerRemovalLimitNumerator;
-    uint256 private ownerRemovalLimitDenominator;
-    Group[] private subgroups;
-    mapping(bytes32 => uint256) private subgroupIndexes;
-    address[] private owners;
-    mapping(address => uint256) private ownerIndexes;
-    mapping(address => OwnerRemovalVoter) private ownerRemovalVoters;
-    mapping(address => uint256) private ownerRemovalVoteCounts;
-    User[] private members;
-    mapping(bytes32 => mapping(address => uint256)) private memberIndexes;
+    struct Storage {
+        uint256 ownerRemovalLimitNumerator;
+        uint256 ownerRemovalLimitDenominator;
+        Group[] subgroups;
+        mapping(bytes32 => uint256) subgroupIndexes;
+        address[] owners;
+        mapping(address => uint256) ownerIndexes;
+        mapping(address => OwnerRemovalVoter) ownerRemovalVoters;
+        mapping(address => uint256) ownerRemovalVoteCounts;
+        User[] members;
+        mapping(bytes32 => mapping(address => uint256)) memberIndexes;
+    }
+
+    function createGroup(Storage storage s, address parent, bytes32 name, address[] memory owners) public returns(Group);
+    function addSubgroup(Storage storage s, bytes32 name, address[] memory owners) public;
+    function checkSubgroup(Storage storage s, bytes32 name) public view returns(bool);
+    function countSubgroups(Storage storage s) public view returns(uint256);
+    function getSubgroup(Storage storage s, uint256 index) public view returns(Group);
+    function removeSubgroup(Storage storage s, bytes32 name) public;
+    function addOwner(Storage storage s, address addr) public;
+    function checkOwner(Storage storage s, address addr) public view returns(bool);
+    function countOwners(Storage storage s) public view returns(uint256);
+    function getOwner(Storage storage s, uint256 index) public view returns(address);
+    function checkOwnerRemovalVote(Storage storage s, address addr) public view returns(bool);
+    function voteToRemoveOwner(Storage storage s, address addr) public;
+    function withdrawVoteToRemoveOwner(Storage storage s, address addr) public;
+    function addMember(Storage storage s, bytes32 role, address addr) public;
+    function checkMember(Storage storage s, bytes32 role, address addr) public view returns(bool);
+    function countMembers(Storage storage s) public view returns(uint256);
+    function getMember(Storage storage s, uint256 index) public view returns(bytes32, address);
+    function removeMember(Storage storage s, bytes32 role, address addr) public;
+}
+
+contract Root {
+
+    RootInterface.Storage s;
+
+    using RootInterface for RootInterface.Storage;
 
     event GroupCreated(address addr);
 
-    constructor(uint256 _ownerRemovalLimitNumerator, uint256 _ownerRemovalLimitDenominator, address[] memory _owners) public {
-        ownerRemovalLimitNumerator = _ownerRemovalLimitNumerator;
-        ownerRemovalLimitDenominator = _ownerRemovalLimitDenominator;
-        for (uint i = 0; i < _owners.length; i++) {
-            address addr = _owners[i];
-            ownerIndexes[addr] = owners.push(addr);
+    constructor(uint256 ownerRemovalLimitNumerator, uint256 ownerRemovalLimitDenominator, address[] memory owners) public {
+        s.ownerRemovalLimitNumerator = ownerRemovalLimitNumerator;
+        s.ownerRemovalLimitDenominator = ownerRemovalLimitDenominator;
+        for (uint i = 0; i < owners.length; i++) {
+            address addr = owners[i];
+            s.ownerIndexes[addr] = s.owners.push(addr);
         }
     }
 
     modifier onlyOwners() {
-        require(checkOwner(msg.sender), "Caller is not owner.");
+        require(s.checkOwner(msg.sender), "Caller is not owner.");
         _;
     }
 
-    function createGroup(address parent, bytes32 name, address[] memory _owners) public returns(Group) {
+    function createGroup(address parent, bytes32 name, address[] memory owners) public returns(Group) {
+        return s.createGroup(parent, name, owners);
+    }
+
+    function addSubgroup(bytes32 name, address[] memory owners) public onlyOwners {
+        return s.addSubgroup(name, owners);
+    }
+
+    function checkSubgroup(bytes32 name) public view returns(bool) {
+        return s.checkSubgroup(name);
+    }
+
+    function countSubgroups() public view returns(uint256) {
+        return s.countSubgroups();
+    }
+
+    function getSubgroup(uint256 index) public view returns(Group) {
+        return s.getSubgroup(index);
+    }
+
+    function removeSubgroup(bytes32 name) public onlyOwners {
+        return s.removeSubgroup(name);
+    }
+
+    function addOwner(address addr) public onlyOwners {
+        return s.addOwner(addr);
+    }
+
+    function checkOwner(address addr) public view returns(bool) {
+        return s.checkOwner(addr);
+    }
+
+    function countOwners() public view onlyOwners returns(uint256) {
+        return s.countOwners();
+    }
+
+    function getOwner(uint256 index) public view onlyOwners returns(address) {
+        return s.getOwner(index);
+    }
+
+    function checkOwnerRemovalVote(address addr) public view onlyOwners returns(bool) {
+        return s.checkOwnerRemovalVote(addr);
+    }
+
+    function voteToRemoveOwner(address addr) public onlyOwners {
+        return s.voteToRemoveOwner(addr);
+    }
+
+    function withdrawVoteToRemoveOwner(address addr) public onlyOwners {
+        return s.withdrawVoteToRemoveOwner(addr);
+    }
+
+    function addMember(bytes32 role, address addr) public onlyOwners {
+        return s.addMember(role, addr);
+    }
+
+    function checkMember(bytes32 role, address addr) public view returns(bool) {
+        return s.checkMember(role, addr);
+    }
+
+    function countMembers() public view returns(uint256) {
+        return s.countMembers();
+    }
+
+    function getMember(uint256 index) public view returns(bytes32, address) {
+        return s.getMember(index);
+    }
+
+    function removeMember(bytes32 role, address addr) public onlyOwners {
+        return s.removeMember(role, addr);
+    }
+}
+
+library RootImplementation {
+
+    event GroupCreated(address addr);
+
+    function createGroup(RootInterface.Storage storage s, address parent, bytes32 name, address[] memory owners) public returns(Group) {
         uint256 ownerCount = 0;
-        for (uint256 i = 0; i < _owners.length; i++) {
-            if (checkMember("Member", _owners[i])) {
+        for (uint256 i = 0; i < owners.length; i++) {
+            if (checkMember(s, "Member", owners[i])) {
                 ownerCount++;
             }
         }
         require(ownerCount > 0, "There is no valid owner.");
         address[] memory validOwners = new address[](ownerCount);
         ownerCount = 0;
-        for (uint256 i = 0; i < _owners.length; i++) {
-            if (checkMember("Member", _owners[i])) {
-                validOwners[ownerCount++] = _owners[i];
+        for (uint256 i = 0; i < owners.length; i++) {
+            if (checkMember(s, "Member", owners[i])) {
+                validOwners[ownerCount++] = owners[i];
             }
         }
-        Group g = new Group(address(this), parent, name, ownerRemovalLimitNumerator, ownerRemovalLimitDenominator, validOwners);
+        Group g = new Group(address(this), parent, name, s.ownerRemovalLimitNumerator, s.ownerRemovalLimitDenominator, validOwners);
         emit GroupCreated(address(g));
         return g;
     }
 
-    function addSubgroup(bytes32 name, address[] memory _owners) public onlyOwners {
-        require(!checkSubgroup(name), "This subgroup already exists.");
-        subgroupIndexes[name] = subgroups.push(createGroup(address(this), name, _owners));
+    function addSubgroup(RootInterface.Storage storage s, bytes32 name, address[] memory owners) public {
+        require(!checkSubgroup(s, name), "This subgroup already exists.");
+        s.subgroupIndexes[name] = s.subgroups.push(createGroup(s, address(this), name, owners));
     }
 
-    function checkSubgroup(bytes32 name) public view returns(bool) {
-        return subgroupIndexes[name] > 0 && subgroups[subgroupIndexes[name] - 1].getName() == name;
+    function checkSubgroup(RootInterface.Storage storage s, bytes32 name) public view returns(bool) {
+        return s.subgroupIndexes[name] > 0 && s.subgroups[s.subgroupIndexes[name] - 1].getName() == name;
     }
 
-    function countSubgroups() public view returns(uint256) {
-        return subgroups.length;
+    function countSubgroups(RootInterface.Storage storage s) public view returns(uint256) {
+        return s.subgroups.length;
     }
 
-    function getSubgroup(uint256 index) public view returns(Group) {
-        require(index < subgroups.length, "This subgroup does not exist.");
-        return subgroups[index];
+    function getSubgroup(RootInterface.Storage storage s, uint256 index) public view returns(Group) {
+        require(index < s.subgroups.length, "This subgroup does not exist.");
+        return s.subgroups[index];
     }
 
-    function removeSubgroup(bytes32 name) public onlyOwners {
-        require(checkSubgroup(name), "This subgroup does not exist.");
-        subgroups[subgroupIndexes[name] - 1] = subgroups[subgroups.length - 1];
-        delete subgroupIndexes[name];
-        delete subgroups[subgroups.length - 1];
-        subgroups.length--;
+    function removeSubgroup(RootInterface.Storage storage s, bytes32 name) public {
+        require(checkSubgroup(s, name), "This subgroup does not exist.");
+        s.subgroups[s.subgroupIndexes[name] - 1] = s.subgroups[s.subgroups.length - 1];
+        delete s.subgroupIndexes[name];
+        delete s.subgroups[s.subgroups.length - 1];
+        s.subgroups.length--;
     }
 
-    function addOwner(address addr) public onlyOwners {
-        require(!checkOwner(addr), "This owner already exists.");
-        ownerIndexes[addr] = owners.push(addr);
+    function addOwner(RootInterface.Storage storage s, address addr) public {
+        require(!checkOwner(s, addr), "This owner already exists.");
+        s.ownerIndexes[addr] = s.owners.push(addr);
     }
 
-    function checkOwner(address addr) public view returns(bool) {
-        return ownerIndexes[addr] > 0 && owners[ownerIndexes[addr] - 1] == addr;
+    function checkOwner(RootInterface.Storage storage s, address addr) public view returns(bool) {
+        return s.ownerIndexes[addr] > 0 && s.owners[s.ownerIndexes[addr] - 1] == addr;
     }
 
-    function countOwners() public view onlyOwners returns(uint256) {
-        return owners.length;
+    function countOwners(RootInterface.Storage storage s) public view returns(uint256) {
+        return s.owners.length;
     }
 
-    function getOwner(uint256 index) public view onlyOwners returns(address) {
-        require(index < owners.length, "This owner does not exist.");
-        return owners[index];
+    function getOwner(RootInterface.Storage storage s, uint256 index) public view returns(address) {
+        require(index < s.owners.length, "This owner does not exist.");
+        return s.owners[index];
     }
 
-    function checkOwnerRemovalVote(address addr) public view onlyOwners returns(bool) {
-        return ownerRemovalVoters[addr].voters[msg.sender];
+    function checkOwnerRemovalVote(RootInterface.Storage storage s, address addr) public view returns(bool) {
+        return s.ownerRemovalVoters[addr].voters[msg.sender];
     }
 
-    function voteToRemoveOwner(address addr) public onlyOwners {
-        require(checkOwner(addr), "This owner does not exist.");
-        require(!checkOwnerRemovalVote(addr), "This vote already exists.");
-        ownerRemovalVoters[addr].voters[msg.sender] = true;
-        ownerRemovalVoteCounts[addr]++;
-        if (owners.length * ownerRemovalLimitNumerator / ownerRemovalLimitDenominator <= ownerRemovalVoteCounts[addr]) {
-            owners[ownerIndexes[addr] - 1] = owners[owners.length - 1];
-            delete ownerIndexes[addr];
-            delete owners[owners.length - 1];
-            owners.length--;
-            delete ownerRemovalVoters[addr];
-            delete ownerRemovalVoteCounts[addr];
+    function voteToRemoveOwner(RootInterface.Storage storage s, address addr) public {
+        require(checkOwner(s, addr), "This owner does not exist.");
+        require(!checkOwnerRemovalVote(s, addr), "This vote already exists.");
+        s.ownerRemovalVoters[addr].voters[msg.sender] = true;
+        s.ownerRemovalVoteCounts[addr]++;
+        if (s.owners.length * s.ownerRemovalLimitNumerator / s.ownerRemovalLimitDenominator <= s.ownerRemovalVoteCounts[addr]) {
+            s.owners[s.ownerIndexes[addr] - 1] = s.owners[s.owners.length - 1];
+            delete s.ownerIndexes[addr];
+            delete s.owners[s.owners.length - 1];
+            s.owners.length--;
+            delete s.ownerRemovalVoters[addr];
+            delete s.ownerRemovalVoteCounts[addr];
         }
     }
 
-    function withdrawVoteToRemoveOwner(address addr) public onlyOwners {
-        require(checkOwner(addr), "This owner does not exist.");
-        require(checkOwnerRemovalVote(addr), "This vote does not exist.");
-        delete ownerRemovalVoters[addr].voters[msg.sender];
-        ownerRemovalVoteCounts[addr]--;
+    function withdrawVoteToRemoveOwner(RootInterface.Storage storage s, address addr) public {
+        require(checkOwner(s, addr), "This owner does not exist.");
+        require(checkOwnerRemovalVote(s, addr), "This vote does not exist.");
+        delete s.ownerRemovalVoters[addr].voters[msg.sender];
+        s.ownerRemovalVoteCounts[addr]--;
     }
 
-    function addMember(bytes32 role, address addr) public onlyOwners {
-        require(!checkMember(role, addr), "This member already exists.");
-        memberIndexes[role][addr] = members.push(User(role, addr));
+    function addMember(RootInterface.Storage storage s, bytes32 role, address addr) public {
+        require(!checkMember(s, role, addr), "This member already exists.");
+        s.memberIndexes[role][addr] = s.members.push(RootInterface.User(role, addr));
     }
 
-    function checkMember(bytes32 role, address addr) public view returns(bool) {
-        return memberIndexes[role][addr] > 0 && members[memberIndexes[role][addr] - 1].addr == addr;
+    function checkMember(RootInterface.Storage storage s, bytes32 role, address addr) public view returns(bool) {
+        return s.memberIndexes[role][addr] > 0 && s.members[s.memberIndexes[role][addr] - 1].addr == addr;
     }
 
-    function countMembers() public view returns(uint256) {
-        return members.length;
+    function countMembers(RootInterface.Storage storage s) public view returns(uint256) {
+        return s.members.length;
     }
 
-    function getMember(uint256 index) public view returns(bytes32, address) {
-        require(index < members.length, "This member does not exist.");
-        return (members[index].role, members[index].addr);
+    function getMember(RootInterface.Storage storage s, uint256 index) public view returns(bytes32, address) {
+        require(index < s.members.length, "This member does not exist.");
+        return (s.members[index].role, s.members[index].addr);
     }
 
-    function removeMember(bytes32 role, address addr) public onlyOwners {
-        require(checkMember(role, addr), "This member does not exist.");
-        members[memberIndexes[role][addr] - 1] = members[members.length - 1];
-        delete memberIndexes[role][addr];
-        delete members[members.length - 1];
-        members.length--;
+    function removeMember(RootInterface.Storage storage s, bytes32 role, address addr) public {
+        require(checkMember(s, role, addr), "This member does not exist.");
+        s.members[s.memberIndexes[role][addr] - 1] = s.members[s.members.length - 1];
+        delete s.memberIndexes[role][addr];
+        delete s.members[s.members.length - 1];
+        s.members.length--;
     }
 }
--- contract/migrations/2_deploy_contracts.js
+++ contract/migrations/2_deploy_contracts.js
@@ -1,5 +1,23 @@
+const DispatcherStorage = artifacts.require('DispatcherStorage');
+const Dispatcher = artifacts.require('Dispatcher');
 const Root = artifacts.require('Root');
+const RootImplementation = artifacts.require('RootImplementation');
 
 module.exports = async (deployer, network, accounts) => {
-  deployer.deploy(Root, 2, 5, [accounts[0]]);
+  let rootImplementation, dispatcherStorage, dispatcher;
+  await deployer.deploy(RootImplementation).then(instance => {
+    rootImplementation = instance;
+  });
+  await deployer.deploy(DispatcherStorage, [accounts[0]]).then(instance => {
+    dispatcherStorage = instance;
+    dispatcherStorage.upgrade(rootImplementation.address, { from: accounts[0] });
+    Dispatcher.unlinked_binary = Dispatcher.unlinked_binary
+      .replace('1111222233334444555566667777888899990000',
+        dispatcherStorage.address.slice(2));
+  });
+  await deployer.deploy(Dispatcher).then(instance => {
+    dispatcher = instance;
+    Root.link('RootInterface', dispatcher.address);
+  });
+  await deployer.deploy(Root, 2, 5, [accounts[0]]);
 };
